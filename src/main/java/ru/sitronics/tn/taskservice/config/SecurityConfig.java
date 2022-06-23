cdd4e63548183a9f334f63465220136dbceea990
Покровский Константин Сергеевич <kpokrovskiy@sitronics.com>
2022-06-23 18:58:17 +0300

    added CorsFilterConfig for front devs

--- src/main/java/ru/sitronics/tn/taskservice/config/SecurityConfig.java
+++ src/main/java/ru/sitronics/tn/taskservice/config/SecurityConfig.java
@@ -3,14 +3,38 @@ package ru.sitronics.tn.taskservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().and().antMatcher("/**").anonymous();
+        http.csrf().disable().cors()
+                .and().sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().antMatcher("/**").anonymous();
 //        http.csrf().disable().cors().and().authorizeRequests().antMatchers("/**").permitAll();
         return http.build();
+    }
+
+//  while develop front on local machines
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PATCH");
+        config.addAllowedMethod("DELETE");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
